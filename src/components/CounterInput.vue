9171eacab6eb45adee122d040478e91368799eee
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-02-19 18:21:51 +0100

    Add function to handle boba selection and use v-for to loop over array for different flavors

--- src/components/CounterInput.vue
+++ src/components/CounterInput.vue
@@ -1,21 +1,64 @@
 <template>
-  <div class="flex flex-col items-center">
-    <label for="boba_xxx" class="">Tapioca </label>
+  <div class="flex flex-col items-center" :key="props.title">
+    <label :for="`boba-${props.title}`" class="">{{ props.title }} </label>
     <div class="">
-      <button class="btn text-2xl text-center size-9">-</button>
+      <button
+        class="btn text-2xl text-center size-9"
+        @click.prevent="decrease"
+        :disabled="model?.amount === 0 || model?.amount === undefined"
+      >
+        -
+      </button>
       <input
         type="text"
-        value="0"
         disabled
-        name="price"
-        id="price"
+        :name="`boba-${props.title.toLocaleLowerCase()}`"
+        :id="`${props.title}`"
         class="input input-bordered text-center size-9 disabled:cursor-default"
+        v-model="computedModel"
       />
-      <button class="btn text-2xl text-center size-9">+</button>
+      <button
+        class="btn text-2xl text-center size-9"
+        @click.prevent="increase"
+        :disabled="model?.amount === 4 || locked"
+      >
+        +
+      </button>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+
+const model = defineModel();
+const emits = defineEmits(["change", "click"]);
+const props = defineProps({
+  title: String,
+  locked: Boolean,
+});
+
+const computedModel = computed({
+  get: () => (model.value?.amount !== undefined ? model.value.amount : 0),
+  set: (value) => {
+    model.value = value;
+  },
+});
+const list = ref({ name: props.title, amount: 0 });
+const decrease = computed(() => {
+  list.value.amount--;
+  model.value = list.value;
+  emits("change", model.value);
+  emits("click", "sub");
+  console.log(model.value.amount);
+});
+const increase = computed(() => {
+  list.value.amount++;
+  model.value = list.value;
+  emits("change", model.value);
+  emits("click", "add");
+  console.log(model.value.amount);
+});
+</script>
 
 <style scoped></style>
--- src/components/views/Buy.vue
+++ src/components/views/Buy.vue
@@ -17,32 +17,49 @@
             class="input input-bordered w-full max-w-xs"
             v-model="composed.name"
         /></label>
+
+        <!-- SIRUP FLAVORS -->
         <label for="sirup" class="label">
           Sirup:
-          <!-- Ajouter logique for loop pour les sirops quand api est pret -->
+
           <select
             id="sirup"
             class="select select-bordered w-full max-w-xs"
             v-model="composed.sirup"
           >
             <option disabled selected>Choose your sirup</option>
-            <option value="peach">Peach</option>
-            <option value="grape">Grape</option>
-            <option value="strawberry">Strawberry</option>
-            <option value="lemon">Lemon</option>
-            <option value="orange">Orange</option>
+            <option v-for="sirup in sirupFlavors" :value="sirup">
+              {{ sirup }}
+            </option>
           </select>
         </label>
-        <div class="grid grid-cols-4 gap-4">
-          <CounterInput title="Tapioca" v-model="composed.boba" />
-          <CounterInput title="Strawberry" />
-          <CounterInput title="Peach" />
-          <CounterInput title="Litchi" />
-          <CounterInput title="Grape" />
-          <CounterInput title="Apple" />
-          <CounterInput title="Lemon" />
-          <CounterInput title="Sugar" />
+        <!-- ------------------------------------------------------------- -->
+        <!-- TEA FLAVORS -->
+        <div for="tea" class="flex flex-row w-full">
+          <label class="w-fit" for="">Tea:</label>
+          <div class="flex justify-evenly w-full">
+            <RadioInput
+              v-for="tea in teaFlavors"
+              :key="tea.name"
+              :title="tea.name"
+              :color="tea.color"
+              v-model="composed.tea"
+            />
+          </div>
         </div>
+        <!-- ------------------------------------------------------------- -->
+        <!-- BOBA FLAVORS -->
+        <div class="grid grid-cols-4 gap-4">
+          <CounterInput
+            v-for="boba in bobaFlavors"
+            :title="boba"
+            :locked="bobaLock"
+            :key="boba"
+            @change="(value) => addBoba(value)"
+            @click="(type) => changeAmount(type)"
+          />
+        </div>
+        <!-- ------------------------------------------------------------- -->
         <RangeInput
           title="Sweetness"
           :min="-5"
@@ -60,6 +77,13 @@
           :scale="['No Ice', 'Regular', 'Lots of Ice']"
           v-model="composed.ice"
         />
+
+        <input
+          type="submit"
+          value="Submit your order"
+          class="btn btn-primary"
+          :disabled="submitable"
+        />
       </form>
     </div>
     {{ composed }}
@@ -69,18 +93,87 @@
 <script setup>
 import CounterInput from "../CounterInput.vue";
 import RangeInput from "../RangeInput.vue";
+import RadioInput from "../RadioInput.vue";
 import { ref, computed } from "vue";
+
+const bobaFlavors = [
+  "Tapioca",
+  "Sugar",
+  "Peach",
+  "Grape",
+  "Apple",
+  "Lemon",
+  "Litchi",
+];
+
+const sirupFlavors = [
+  "Peach",
+  "Grape",
+  "Strawberry",
+  "Lemon",
+  "Orange",
+  "Pear",
+  "Melon",
+  "Cherry",
+  "Watermelon",
+];
+
+const teaFlavors = [
+  { name: "Green", color: "#00e000" },
+  { name: "Black", color: "#000" },
+  { name: "White", color: "#fff" },
+  { name: "Blue", color: "#0000e0" },
+];
 
 const composed = ref({
   name: "",
   sirup: "",
+  tea: "",
   boba: [],
   sweetness: 0,
   ice: 0,
 });
 
-computed(() => {
-  console.log(composed.value);
+const maxBoba = 4;
+const bobaCount = ref(0);
+const bobaLock = ref(false);
+
+const addBoba = (value) => {
+  if (
+    composed.value.boba.length < maxBoba &&
+    !composed.value.boba.some((boba) => boba.name === value.name)
+  ) {
+    composed.value.boba.push(value);
+  }
+  composed.value.boba.forEach((boba, index) => {
+    if (boba.amount === 0) {
+      composed.value.boba.splice(index, 1);
+    }
+  });
+};
+
+const changeAmount = (type) => {
+  if (type === "sub") {
+    bobaCount.value--;
+  } else {
+    bobaCount.value++;
+  }
+  if (bobaCount.value === maxBoba) {
+    bobaLock.value = true;
+  } else {
+    bobaLock.value = false;
+  }
+};
+
+const submitable = computed(() => {
+  if (
+    composed.value.name.length < 3 ||
+    composed.value.sirup.length === 0 ||
+    composed.value.tea.length === 0
+  ) {
+    return true;
+  }
+  return false;
 });
 </script>
 
