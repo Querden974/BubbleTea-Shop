4fe0c6ff4954c66bd46f2903ad5ac5f091c0df8e
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-02-25 12:36:26 +0100

    Implement color transition when changing tea flavor

--- src/components/functions/Viz.vue
+++ src/components/functions/Viz.vue
@@ -1,4 +1,7 @@
 <template>
+  <!-- <p>{{ colors.teas.find((a) => a.name === "black").color }}</p> -->
+  <p>current : {{ currColor }}</p>
+  <p>next : {{ nextColor ? nextColor : "null" }}</p>
   <div
     v-show="true"
     ref="threeContainer"
@@ -8,21 +11,30 @@
 
 <script setup>
 import * as THREE from "three";
+import * as TWEEN from "@tweenjs/tween.js";
 import { Geometry, ImportGLTF, Initialize } from "./3DViz";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { inject, onMounted, ref } from "vue";
+import { computed, inject, onMounted, ref, watch } from "vue";
 
 const threeContainer = ref();
 const size = ref();
 const composed = inject("composed");
+const colors = inject("colors");
+
+const nextColor = computed(
+  () => colors.value?.teas.find((a) => a.name === composed.value.tea)?.color
+);
+const currColor = ref(
+  colors.value?.teas.find((a) => a.name === composed.value.tea)?.color
+);
 
 const init = new Initialize();
-init.renderer.setAnimationLoop(animate);
+// init.renderer.setAnimationLoop(animate);
 const scene = init.scene;
 
 const localPlane = new THREE.Plane(new THREE.Vector3(0, -1, 0), -0.1);
 const helper = new THREE.PlaneHelper(localPlane, 3, 0xffff00);
-scene.add(helper);
+// scene.add(helper);
 
 //---------------- IMPORT CUP --------------------//
 
@@ -34,12 +46,52 @@ importCup.material.transparent = true;
 importCup.material.opacity = 0.05;
 
 //---------------- IMPORT TEA --------------------//
+
 const importTea = new ImportGLTF();
 importTea.material.clippingPlanes = [localPlane];
 importTea.material.clipIntersection = true;
+importTea.material.transmission = 1;
+importTea.material.transparent = true;
+importTea.material.opacity = 0.3;
+
 importTea
   .loadModel(import.meta.env.VITE_OBJ_TEA, 0.1)
   .then((model) => scene.add(model));
+
+const tweenTea = new TWEEN.Tween({ x: 0 })
+  .to({ x: 1 }, 1000)
+  .onUpdate(({ x }) => {
+    importTea.material.color.lerpColors(
+      importTea.material.color,
+      new THREE.Color(nextColor.value),
+      x
+    );
+  })
+  .onComplete(() => {
+    pauseRender();
+  });
+
+const tweenLocalPlane = new TWEEN.Tween({ x: 0 })
+  .to({ x: 2.75 }, 1000)
+  .onStart(() => {
+    resumeRender();
+  })
+  .onUpdate(({ x }) => {
+    localPlane.constant = x;
+  })
+  .onComplete(() => {
+    pauseRender();
+  });
+
+let teaVisible = false;
+watch(nextColor, () => {
+  resumeRender();
+  if (!teaVisible) {
+    tweenLocalPlane.start();
+    teaVisible = true;
+  }
+  tweenTea.start();
+});
 
 //---------------- CREATE LIGHTS --------------------//
 
@@ -50,25 +102,30 @@ pointLight.position.set(0, 1, 3);
 scene.add(pointLight);
 
 //------------------------------------------------//
-
-function animate() {
-  if (composed.value.tea.length !== 0) {
-    if (composed.value.tea === "black") {
-      importTea.material.color = new THREE.Color("#3B2F2F");
-      importTea.material.emissiveIntensity = 100;
-    } else if (composed.value.tea === "green") {
-      importTea.material.color = new THREE.Color("#A3C47D");
-    } else if (composed.value.tea === "white") {
-      importTea.material.color = new THREE.Color("#F8F5E1");
-    } else if (composed.value.tea === "blue") {
-      importTea.material.color = new THREE.Color("#6C7A89");
-    }
-
-    if (localPlane.constant < 2.75) {
-      localPlane.constant += 0.02;
-    }
+let isPaused = true;
+let initRenderView = false;
+function animate(time) {
+  if (!initRenderView) {
+    setTimeout(() => {
+      init.renderer.render(scene, init.camera);
+      initRenderView = true;
+    }, 100);
   }
-  init.renderer.render(scene, init.camera);
+  if (!isPaused) {
+    const requestID = window.requestAnimationFrame(animate);
+    TWEEN.update(time);
+    init.renderer.render(scene, init.camera);
+    console.log(time);
+  }
+}
+function pauseRender() {
+  isPaused = true;
+}
+function resumeRender() {
+  if (isPaused) {
+    isPaused = false;
+    animate();
+  }
 }
 
 onMounted(() => {
